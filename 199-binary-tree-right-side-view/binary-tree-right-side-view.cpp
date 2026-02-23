49344e593627710f6bc30ef9955981afbbb13f31
Rohan Karak <karakrohan2005@gmail.com>
2026-02-23 19:08:30 +0530

    Time: 0 ms (100.00%) | Memory: 14.8 MB (84.68%) - LeetSync

---
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    // Int level name ka ek function bana diya
+    int levels(TreeNode* root){
+        if(root==NULL) return 0;
+        return 1+max(levels(root->left), levels(root->right));
+    }
+
+    void nthLevelRev(TreeNode* root, int curr, int level, vector<int>& ans){
+        if(root==NULL) return; // base case
+        if(curr==level){
+            ans[level] = root->val; // root
+            return;
+        }
+        nthLevelRev(root->left, curr+1, level, ans); // left
+        nthLevelRev(root->right, curr+1, level, ans); // right
+    }
+
+    void levelOrder(TreeNode* root, vector<int>& ans){
+        int n = ans.size();
+        for(int i=0; i<n; i++){
+            nthLevelRev (root,0,i, ans);
+        }
+    }
+
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> ans(levels(root),0);
+        levelOrder(root,ans);
+        return ans;
+    }
+}; 
