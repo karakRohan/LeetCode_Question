c1ab2865f0285ad82fae1e2cc87eaed6ab66e15c
Rohan Karak <karakrohan2005@gmail.com>
2026-02-25 18:15:31 +0530

    Time: 25 ms (40.57%) | Memory: 23.3 MB (90.52%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -1,19 +1,11 @@
-// Bad Method :- 1
+// Method :- 2 sahi Method
 class Solution {
 public:
-    bool exists(TreeNode* root, TreeNode* target) {
-        if (root == NULL)
-            return false;
-        if (root == target)
-            return true;
-        return exists(root->left, target) || exists(root->right, target);
-    }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if (exists(root->left, p) && exists(root->left, q))
-            return lowestCommonAncestor(root->left, p, q);
-        else if
-            (exists(root->right, p) && exists(root->right, q))
-            return lowestCommonAncestor(root->right, p, q);
-            return root;
+        if(root->val==p->val || root->val==q->val) return root;
+        if(root->val > p->val && root->val < q->val) return root;
+        else if(root->val < p->val && root->val > q->val) return root; 
+        else if (root->val < p->val && root->val < q->val) return lowestCommonAncestor(root->right,p,q);
+        else return lowestCommonAncestor(root->left,p,q);
     }
 };
