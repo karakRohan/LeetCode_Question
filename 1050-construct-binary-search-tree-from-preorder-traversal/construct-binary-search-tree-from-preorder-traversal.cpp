c9fc344bac99c30706131896bde371b2068c64e7
Rohan Karak <karakrohan2005@gmail.com>
2026-02-26 13:30:10 +0530

    Time: 0 ms (100.00%) | Memory: 15.9 MB (90.74%) - LeetSync

--- 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.cpp
@@ -1,26 +1,28 @@
-// M :- 1 (Beker Method) 
+//Method :- 2
 class Solution {
 public:
 
-    TreeNode* build(vector<int>& pre, int prelo, int prehi, vector<int>& in, int inlo, int inhi){
-        if(prelo>prehi) return NULL;
-        TreeNode* root = new TreeNode(pre[prelo]);
-        if(prelo == prehi) return root;
-        int i = inlo;
-        while(i<=inhi) {
-            if(in[i]==pre[prelo]) break;
-            i++;
+    void insert(TreeNode* root, int val){
+        if(root==NULL) root = new TreeNode(val);
+        else if(root->val > val){ // go left
+            if(root -> left == NULL) { // Attach it here
+                root -> left =  new TreeNode(val);
+            }
+            else insert (root->left, val);
         }
-        int leftCount = i - inlo, rightCount = inhi - i;
-        root -> left = build(pre, prelo+1, prelo+leftCount, in, inlo, i-1);
-        root -> right = build(pre, prelo+leftCount+1, prehi, in, i+1, inhi);
-        return root;
+        else{ // root - > val < val : go right
+            if(root -> right == NULL) { // Attach it here
+                root -> right =  new TreeNode(val);
+            }
+            else insert (root->right, val);
+        }
     }
 
-    TreeNode* bstFromPreorder(vector<int>& pre ) {
-        vector<int> in = pre; // copy ban gayi
-        sort(in.begin(), in.end());
-        int n = pre.size();
-        return build(pre,0,n-1,in,0,n-1);
+    TreeNode* bstFromPreorder(vector<int>& pre) {
+        TreeNode* root = new TreeNode(pre[0]);
+        for(int i=1; i<pre.size(); i++){
+            insert(root, pre[i]);
+        }
+        return root;
     }
 };
