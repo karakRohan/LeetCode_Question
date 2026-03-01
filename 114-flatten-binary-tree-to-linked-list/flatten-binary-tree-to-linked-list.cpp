1a61aed3b249546a2a9ba9fc12bbfd97264083c2
Rohan Karak <karakrohan2005@gmail.com>
2026-03-02 02:14:40 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (24.28%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.cpp
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.cpp
@@ -1,35 +1,28 @@
-//Method :- 1 (Simple Method)
+//Method :- 3 (Morris Traversal Code) Best Method
 class Solution {
 public:
-    vector<TreeNode*> preorderTraversal(TreeNode* root) {
-        vector<TreeNode*> ans;
-        stack<TreeNode*> st;
-
-        if (root != NULL)
-            st.push(root);
-
-        while (st.size() > 0) {
-            TreeNode* temp = st.top();
-            st.pop();
-            ans.push_back(temp);
-
-            if (temp->right != NULL)
-                st.push(temp->right);
-
-            if (temp->left != NULL)
-                st.push(temp->left);
-        }
-
-        return ans;
-    }
-
     void flatten(TreeNode* root) {
-        vector<TreeNode*> ans = preorderTraversal(root);
-        int n = ans.size();
-
-        for (int i = 0; i < n - 1; i++) {
-            ans[i]->right = ans[i + 1];
-            ans[i]->left = NULL;
+        // Base case Banai age
+        if(root==NULL) return;
+        TreeNode* curr = root;
+        while(curr != NULL){
+            if(curr -> left != NULL){
+                // save the right
+                TreeNode* r = curr -> right;
+                curr -> right = curr->left;
+                // finding pred(Predecessor)
+                TreeNode* pred = curr -> left;
+                while(pred->right != NULL) pred = pred -> right;
+                // Linking korbo aber
+                pred -> right = r;
+                curr = curr->left;
+            }
+            else curr = curr = curr -> right;
+        }
+        TreeNode* temp = root;
+        while(temp->right != NULL){
+            temp -> left = NULL;
+            temp = temp -> right;
         }
     }
 };
