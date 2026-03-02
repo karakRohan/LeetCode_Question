1b59fd4efbac9d91aede9a63c920579d69aba7fa
Rohan Karak <karakrohan2005@gmail.com>
2026-03-02 13:19:29 +0530

    Time: 7 ms (14.57%) | Memory: 25.8 MB (32.77%) - LeetSync

--- 2847-find-maximum-number-of-string-pairs/find-maximum-number-of-string-pairs.cpp
+++ 2847-find-maximum-number-of-string-pairs/find-maximum-number-of-string-pairs.cpp
@@ -1,15 +1,20 @@
-// Method - 1 [O(n²)]
+// Method - 2 [Using Sets]
 class Solution {
 public:
     int maximumNumberOfStringPairs(vector<string>& arr ) {
         int n = arr.size();
         int count = 0;
-        //unordered_set<int>
-        for(int i = 0; i<n-1;i++){
+        unordered_set<string> s;
+        for(int i = 0; i<n;i++){
+            s.insert(arr[i]);
+        }
+        for(int i = 0; i<n;i++){
             string rev = arr[i];
             reverse(rev.begin(), rev.end());
-            for(int j=i+1; j<n; j++){
-                if(rev==arr[j]) count++;
+            if(arr[i]==rev) continue;
+            if(s.find(rev) != s.end()){
+                count++;
+                s.erase(arr[i]);
             }
         }
         return count;
