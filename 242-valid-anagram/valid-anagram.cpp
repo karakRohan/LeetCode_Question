ecdaa1475ae0459b280aedb9ce3b3b0b835ada5c
Rohan Karak <karakrohan2005@gmail.com>
2026-03-02 16:34:32 +0530

    Time: 1 ms (55.44%) | Memory: 9.5 MB (91.32%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,10 @@
+// Method 1 (Solution in 'String')
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        sort(s.begin(), s.end());
+        sort(t.begin(),t.end());
+        if(s==t) return true;
+        else return false;
+    }
+};
