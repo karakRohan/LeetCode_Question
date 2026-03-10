6ea06abbabba3a6ee851ee153d5d970353cb4f0d
Rohan Karak <karakrohan2005@gmail.com>
2026-03-10 13:03:28 +0530

    Time: 72 ms (72.15%) | Memory: 36.6 MB (95.59%) - LeetSync

---
+++ 632-smallest-range-covering-elements-from-k-lists/smallest-range-covering-elements-from-k-lists.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    typedef pair<int, pair<int,int>> pip; // pair of int pair
+    vector<int> smallestRange(vector<vector<int>>& arr) {
+        priority_queue<pip, vector<pip>, greater<pip>> pq;
+        // pq element -> {arr[row][column],{row, column}}
+        int mx = INT_MIN;
+        for(int i=0;i<arr.size();i++){
+            mx = max(mx,arr[i][0]);
+            pq.push({arr[i][0],{i,0}});
+        }
+        int mn = pq.top().first;
+        int start = mn, end = mx;
+        while(true){
+            int row = pq.top().second.first;
+            int col = pq.top().second.second;
+            pq.pop();
+            if(col == arr[row].size()-1) break;
+            pq.push({arr[row] [col+1], {row,col+1}});
+            mx = max(mx,arr[row][col+1]); // mx = Maximum
+            mn = pq.top().first; // mn = Minimum
+            if(mx-mn < end-start){
+                end = mx;
+                start = mn;
+            }
+        }
+        return {start, end};
+    }
+};
