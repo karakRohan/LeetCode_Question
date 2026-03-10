8bd49f56df0d3c6fb40c1c8b1dcf842afe178209
Rohan Karak <karakrohan2005@gmail.com>
2026-03-10 13:07:16 +0530

    Time: 71 ms (76.51%) | Memory: 36.8 MB (67.47%) - LeetSync

--- 632-smallest-range-covering-elements-from-k-lists/smallest-range-covering-elements-from-k-lists.cpp
+++ 632-smallest-range-covering-elements-from-k-lists/smallest-range-covering-elements-from-k-lists.cpp
@@ -5,9 +5,9 @@ public:
         priority_queue<pip, vector<pip>, greater<pip>> pq;
         // pq element -> {arr[row][column],{row, column}}
         int mx = INT_MIN;
-        for(int i=0;i<arr.size();i++){
+        for(int i=0;i<arr.size();i++){ // k times (array ka size k )
             mx = max(mx,arr[i][0]);
-            pq.push({arr[i][0],{i,0}});
+            pq.push({arr[i][0],{i,0}}); // O(k logk)
         }
         int mn = pq.top().first;
         int start = mn, end = mx;
@@ -16,7 +16,7 @@ public:
             int col = pq.top().second.second;
             pq.pop();
             if(col == arr[row].size()-1) break;
-            pq.push({arr[row] [col+1], {row,col+1}});
+            pq.push({arr[row] [col+1], {row,col+1}}); // O(nk logk)
             mx = max(mx,arr[row][col+1]); // mx = Maximum
             mn = pq.top().first; // mn = Minimum
             if(mx-mn < end-start){
