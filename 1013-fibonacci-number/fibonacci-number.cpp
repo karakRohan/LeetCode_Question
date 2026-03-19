3cba2ab2926347829958fbba39a0ed6e07042448
Rohan Karak <karakrohan2005@gmail.com>
2026-03-19 21:37:58 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (36.74%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,14 +1,14 @@
-// Method :- Using - Dp
+// Method 3 :- Using - Dp(Tabulation Method) With Out Recurtion
 
 class Solution {
 public:
-    int fibo(int n, vector<int>& dp){
-        if(n<=1) return n;
-        if(dp[n] != -1) return dp[n];
-        return dp[n] = fibo(n-1, dp) + fibo(n-2, dp);
-    }
-    int fib(int n) {
-        vector<int> dp(n+1, -1);
-        return fibo(n, dp);
+    int fib(int n){
+        int dp[n+1];
+        dp[0] = 0;
+        if(n>=1) dp[1] = 1;
+        for(int i = 2; i<= n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
     }
 };
