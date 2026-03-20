fc2bc0fd7170456ec13d6c09ec58d38c4ab9d069
Rohan Karak <karakrohan2005@gmail.com>
2026-03-20 13:23:06 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (97.15%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,17 +1,15 @@
-// Method 1 :- Recursive dp Memoization Code
+// Method 2 :- Tabulation Code
 
 class Solution {
 public:
-    int helper(int sr, int sc, int er, int ec, vector<vector<int>>& dp){
-        if(sr==er && sc==ec) return 1; 
-        // sr = Starting Row, er = Ending Row; sc = Starting Column, ec= Ending Column
-        if(sr > er || sc>ec) return 0;
-        if(dp[sr][sc] != -1) return dp[sr][sc];
-        return dp[sr][sc] = helper(sr,sc+1,er,ec,dp) + helper(sr+1,sc,er,ec,dp);
-    }
-
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m,vector<int>(n,-1));
-        return helper(0,0,m-1,n-1,dp);
+        int dp[m][n]; // SC = O(mn)
+        for(int i = 0; i<m; i++ ){
+            for(int j=0;j<n; j++){
+                if(i==0 || j==0) dp[i][j] = 1;
+                else dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
