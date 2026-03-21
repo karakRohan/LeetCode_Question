b933db57575a2d50de68ca2bfabb5b69a14fd0dd
Rohan Karak <karakrohan2005@gmail.com>
2026-03-21 14:25:24 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (60.89%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,4 +1,4 @@
-// Method :- 1 (Recursive Solution )
+// Method :- 1 (Dp Solution )
 class Solution {
 public:
     vector<int>dp;
@@ -8,7 +8,7 @@ public:
         return max(arr[i] + f(arr, i+2), 0 + f(arr, i+1));
     }
 
-    int ftd(vector<int> arr, int i) {// f = Profit
+    int ftd(vector<int> arr, int i) {// f = Profit // td = top down
         if(i == arr.size()-1) return arr[i]; // single house
         if(i == arr.size()-2) return max(arr[i], arr[i+1]); // 2 houses
 
@@ -18,9 +18,24 @@ public:
         return dp[i] = max(arr[i] + ftd(arr, i+2), 0 + ftd(arr, i+1));
     }
 
-    int rob(vector<int>& nums) {
+    int fbu(vector<int> &arr) { //bu = Bottom Up
+        int n = arr.size();
+        if(n==1) return arr[0];
         dp.clear();
-        dp.resize(105,-1);
-        return ftd(nums, 0);
+        dp.resize(n);
+        // base case
+        dp[n-1] = arr[n-1];
+        dp[n-2] = max(arr[n-1], arr[n-2]);
+        for(int i = n-3; i >= 0; i--){
+            dp[i] = max(arr[i] + dp[i+2], 0 + dp[i+1]);
+        }
+        return dp[0];
+    }
+
+    int rob(vector<int>& nums) {
+        // dp.clear();
+        // dp.resize(105,-1);
+        // return ftd(nums, 0);
+        return fbu(nums);
     }
 };
