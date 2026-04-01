55253ae4fe437882a925fc202e6ea121765f2257
Rohan Karak <karakrohan2005@gmail.com>
2026-04-01 11:58:36 +0530

    Time: 10 ms (5.28%) | Memory: 32.1 MB (15.15%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -13,7 +13,19 @@ public:
         n = arr.size();
         m = arr[0].size();
         dp.clear();
-        dp.resize(205, vector<int> (205, -1));
-        return f(0,0);
+        dp.resize(205, vector<int> (205, 0));
+        dp[n-1][m-1] = arr[n-1][m-1];
+        for(int i = n-2; i>=0; i--){
+            dp[i][m-1] = arr[i][m-1] + dp[i+1][m-1];
+        }
+        for(int i = m-2; i>=0;i--){
+            dp[n-1][i] = arr[n-1][i] + dp[n-1][i+1];
+        }
+        for(int i = n-2; i >= 0; i--){
+            for(int j = m-2; j >= 0; j--){
+                dp[i][j] = arr[i][j] + min(dp[i+1][j], dp[i][j+1]);
+            }
+        }
+        return dp[0][0];
     }
 };
