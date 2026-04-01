f0948e1f2d4a34459b0ccb1ca806b3cd8446c7c5
Rohan Karak <karakrohan2005@gmail.com>
2026-04-01 19:02:16 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (78.61%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -1,21 +1,30 @@
 class Solution {
 public:
     vector<int> dp;
-    int f(int n){
-        if(n==0 or n==1) return 1;
-        if(n==2) return 2;
-        if(dp[n] != -1) return dp[n];
+    int f(int i){
+        if(i==0 or i==1) return 1;
+        if(i==2) return 2;
+        if(dp[i] != -1) return dp[i];
 
         int sum = 0;
-        for(int k = 1; k<=n; k++){
+        for(int k = 1; k<=i; k++){
             // k -> potential root
-            sum += f(k-1)*f(n-k);
+            sum += f(k-1)*f(i-k);
         }
-        return dp[n] = sum;
+        return dp[i] = sum;
     }
     int numTrees(int n) {
         dp.clear();
-        dp.resize(30, -1);
-        return f(n);
+        dp.resize(30, 0);
+        dp[0] = dp[1] = 1;
+        dp[2] = 2;
+        for(int i = 3; i<=n; i++){
+            //f(i)
+            for(int k = 1; k <= i; k++){
+                // k -> potential root
+                dp[i] += dp[k-1]*dp[i-k];
+            }
+        }
+        return dp[n];
     }
 };
