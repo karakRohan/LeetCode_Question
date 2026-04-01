67ab85778a33d4b749e5db066476aded48474d4c
Rohan Karak <karakrohan2005@gmail.com>
2026-04-01 12:21:44 +0530

    Time: 3 ms (9.34%) | Memory: 12.2 MB (5.02%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,15 +1,26 @@
-// Method 2 :- Tabulation Code
+// Method 3 :- Dp (Sanket Singh)
 
 class Solution {
 public:
-    int uniquePaths(int m, int n) {
-        int dp[m][n]; // SC = O(mn)
-        for(int i = 0; i<m; i++ ){
-            for(int j=0;j<n; j++){
-                if(i==0 || j==0) dp[i][j] = 1;
-                else dp[i][j] = dp[i-1][j] + dp[i][j-1];
-            }
-        }
-        return dp[m-1][n-1];
+    int n, m;
+    vector<vector<int>> dp;
+
+    int f(int i, int j) {
+        if (i == n - 1 && j == m - 1) return 1;
+        if (i < 0 || j < 0 || i >= n || j >= m) return 0;
+
+        if (dp[i][j] != -1) return dp[i][j];
+
+        return dp[i][j] = f(i, j + 1) + f(i + 1, j);
+    }
+
+    int uniquePaths(int M, int N) {
+        n = M;
+        m = N;
+
+        dp.clear();
+        dp.resize(105, vector<int>(105, -1));
+
+        return f(0, 0);
     }
 };
