c4a983bd534a0afa81288f31d00c1acf3ecb9fb3
Rohan Karak <karakrohan2005@gmail.com>
2026-04-02 20:51:42 +0530

    Time: 0 ms (100.00%) | Memory: 20 MB (58.98%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,28 +1,15 @@
+// Greedy Approch
+
 class Solution {
 public:
-    int dp[100005][2];
-
-    int f(vector<int> &prices, int i, bool on){
-        if(i == prices.size()) return 0;
-
-        if(dp[i][on] != -1) return dp[i][on];
-
-        int ans = f(prices, i+1, on); // skip
-
-        if(on){
-            // sell
-            ans = max(ans, prices[i] + f(prices, i+1, false));
-        }
-        else{
-            // buy
-            ans = max(ans, f(prices, i+1, true) - prices[i]);
-        }
-
-        return dp[i][on] = ans;
-    }
-
     int maxProfit(vector<int>& prices) {
-        memset(dp, -1, sizeof dp);
-        return f(prices, 0, false);
+        int profit = 0;
+
+        for(int i = 1; i < prices.size(); i++){
+            if(prices[i] > prices[i-1]){
+                profit += prices[i] - prices[i-1];
+            }
+        }
+        return profit;
     }
 };
