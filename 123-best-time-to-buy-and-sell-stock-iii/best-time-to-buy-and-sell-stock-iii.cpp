ca18d095255158be2af26eaf43571873717729a8
Rohan Karak <karakrohan2005@gmail.com>
2026-04-02 19:26:41 +0530

    Time: 76 ms (54.88%) | Memory: 97.2 MB (52.91%) - LeetSync

---
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    //vector<vector<vector<int> > > dp; // 3D dp
+    // Array se banana hai
+    int dp[100005][10][2];
+    int f(vector<int> &prices, int i, int k, bool on){
+        if(i== prices.size()) return 0;
+        if(dp[i][k][on] != -1) return dp[i][k][on];
+
+        int ans = INT_MIN;
+        ans = f(prices, i+1, k, on); // Avoid Cases
+        // Not Avoid 
+        if(on){
+            ans = max(ans,prices[i] + f(prices, i+1, k-1, false));
+        }
+        else{
+            if(k > 0){
+                ans = max(ans, f(prices,i+1,k, true)- prices[i]);
+            }
+        }
+        return dp[i][k][on] = ans;
+    }
+    int maxProfit(vector<int>& prices) {
+        memset(dp, -1, sizeof dp);
+        return f(prices, 0, 2, false);
+    }
+};
