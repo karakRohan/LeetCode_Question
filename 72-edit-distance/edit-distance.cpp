96a8ccf5605b413a0034a0cf48a4017d8d38ee0a
Rohan Karak <karakrohan2005@gmail.com>
2026-04-06 13:33:00 +0530

    Time: 50 ms (12.60%) | Memory: 11.6 MB (87.55%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -11,8 +11,30 @@ public:
         if(s1[i] == s2[j]) return dp[i][j] = f(s1,s2, i+1, j+1);
         return dp[i][j] = min({1+f(s1,s2,i+1,j+1),1 + f(s1,s2,i+1,j), 1 + f(s1,s2,i,j+1)});
     }
-    int minDistance(string word1, string word2) {
-        memset(dp, -1, sizeof dp);
-        return f(word1, word2, 0, 0 );
+    int minDistance(string s1, string s2) {
+        memset(dp, 0, sizeof dp);
+
+        // base case
+        for(int j = 0; j < s2.size(); j++){
+            int i = s1.size();
+            dp[i][j] = s2.size() - j;
+        }
+        for(int i = 0; i < s1.size(); i++){
+            int j = s2.size();
+            dp[i][j] = s1.size() - i;
+        }
+        // Bottom Up
+        for(int i = s1.size() - 1; i >= 0; i--){
+            for(int j = s2.size() - 1; j >= 0; j--){
+                if(s1[i] == s2[j]) {
+                    dp[i][j] = f(s1,s2, i+1, j+1);
+                }
+                else {
+                    dp[i][j] = min({1 + dp[i+1][j+1], 1 + dp[i+1][j], 1 + dp[i][j+1]});
+                }
+            }
+        }
+        //return f(word1, word2, 0, 0 );
+        return dp[0][0];
     }
 };
