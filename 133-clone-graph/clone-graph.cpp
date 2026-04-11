df3e2eb1ddadcda7b9341c6c6c8ff650ed3ffbf1
Rohan Karak <karakrohan2005@gmail.com>
2026-04-11 14:00:20 +0530

    Time: 7 ms (22.65%) | Memory: 12.2 MB (27.86%) - LeetSync

---
+++ 133-clone-graph/clone-graph.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<Node*> nodeRegister;
+
+    void dfs(Node* actual, Node* clone){
+        for(auto neighbor : actual -> neighbors){
+            if(not nodeRegister[neighbor -> val]){
+                // Create the neighbor for the first time
+                Node* newNode = new Node(neighbor -> val);
+                nodeRegister[newNode -> val] = newNode;
+                clone -> neighbors.push_back(newNode);
+                dfs(neighbor, newNode);
+            }
+            else {
+                clone -> neighbors.push_back(nodeRegister[neighbor->val]);
+            }
+        }
+    }
+
+    Node* cloneGraph(Node* node) {
+        if(node == NULL) return NULL;
+        Node* clone = new Node(node -> val);
+        nodeRegister.resize(110, NULL); // This array contains reference to the created nodes
+        nodeRegister[clone -> val] = clone;
+        dfs(node, clone);
+        return clone;
+    }
+};
