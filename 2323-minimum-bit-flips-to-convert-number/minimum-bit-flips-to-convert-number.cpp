1b3d71b193d7caafd2a045f51a646f77b8991eb1
Rohan Karak <karakrohan2005@gmail.com>
2026-04-22 00:26:03 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (92.29%) - LeetSync

--- 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
@@ -1,11 +1,9 @@
 class Solution {
 public:
     int minBitFlips(int start, int goal) {
-        int ans=start^goal; // gives the bit that need to be flip (^ ata Hoccha XOR Oparation )
-        int counter =0;
-
+        int ans=start^goal, counter =0; // gives the bit that need to be flip (^ ata Hoccha XOR Oparation )
         while(ans){
-           ans&=(ans-1); // removing last bit of ans and storing it in ans;
+           ans&=(ans-1); // removing last bit of ans and storing it in ans,(rightmost 1)
            counter++;
         }
         return counter;
