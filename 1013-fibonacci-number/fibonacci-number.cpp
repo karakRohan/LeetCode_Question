f841168d89506973b0eb202b4591876263d94071
Rohan Karak <karakrohan2005@gmail.com>
2026-05-08 13:35:17 +0530

    Time: 13 ms (30.07%) | Memory: 7.7 MB (65.09%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,15 +1,7 @@
-// Method 3 :- Using - Dp(Tabulation Method) With Out Recurtion
-
 class Solution {
 public:
-    int fib(int n){
+    int fib(int n) {
         if(n<=1) return n;
-        int dp[n+1];
-        dp[0] = 0;
-        dp[1] = 1;
-        for(int i = 2; i<= n; i++){
-            dp[i] = dp[i-1] + dp[i-2];
-        }
-        return dp[n];
+        return fib(n-1) + fib(n-2);
     }
 };
