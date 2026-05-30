e4803b377943cc670c3e0908dad85d18932fe307
Rohan Karak <karakrohan2005@gmail.com>
2026-05-31 00:37:32 +0530

    Time: 3 ms (86.98%) | Memory: 8.5 MB (77.72%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -3,7 +3,7 @@ public:
     int countSubstrings(string s) {
         int n = s.size(), ans = 0;
 
-        for (int i = 0; i <= n; i++) {
+        for (int i = 0; i < n; i++) {
             for (int l = i, r = i; l >= 0 && r < n && s[l] == s[r]; l--, r++) ans++;
             for (int l = i, r = i + 1; l >= 0 && r < n && s[l] == s[r]; l--, r++) ans++;
         }
