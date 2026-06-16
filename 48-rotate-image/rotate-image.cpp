e284790694a5329dd4eefff41e6488f17afbb292
Rohan Karak <karakrohan2005@gmail.com>
2026-06-16 23:23:56 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (34.27%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -4,16 +4,8 @@ public:
         int n = m.size();
         // transpose 
         for(int i=0;i<n;i++)
-            for(int j=i+1;j<n;j++)
-                swap(m[i][j], m[j][i]);
+            for(int j=i+1;j<n;j++) swap(m[i][j], m[j][i]);
         // reverse each row (manual swap)
-        for(int i=0;i<n;i++){
-            // int c=0, r=n-1;
-            // while(c<r){
-            //     swap(m[i][c], m[i][r]);
-            //     c++; r--;
-            // }
-            reverse(m[i].begin(), m[i].end());
-        }
+        for(int i=0;i<n;i++) reverse(m[i].begin(), m[i].end());
     }
 };
