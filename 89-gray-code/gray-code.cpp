afca73d3b35e6657a1ec8628d2e1a1b4eb30549f
Rohan Karak <karakrohan2005@gmail.com>
2026-06-22 00:42:13 +0530

    Time: 0 ms (100.00%) | Memory: 14.7 MB (23.96%) - LeetSync

--- 89-gray-code/gray-code.cpp
+++ 89-gray-code/gray-code.cpp
@@ -3,11 +3,9 @@ public:
     vector<int> grayCode(int n) {
         vector<int> ans;
         int size = 1 << n;          // 2^n
-
         for(int i = 0; i < size; i++) {
             ans.push_back(i ^ (i >> 1));
         }
-
         return ans;
     }
 };
