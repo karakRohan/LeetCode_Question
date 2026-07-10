3cfb5849fb13756533ddc02dc71dcea59094e3e3
Rohan Karak <karakrohan2005@gmail.com>
2026-07-10 21:26:07 +0530

    Time: 0 ms (100.00%) | Memory: 23.7 MB (98.90%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.cpp
@@ -0,0 +1,14 @@
+// Striver Code //
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int i = 0, j = 0, n = nums.size();
+        while(j<n){
+            if(nums[j] != 0){
+                swap(nums[i], nums[j]);
+                i++;
+            }
+            j++;
+        }
+    }
+};
