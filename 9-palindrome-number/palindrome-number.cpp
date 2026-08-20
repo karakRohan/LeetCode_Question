52d470e54cc1cba59d8eacec0656d9d825e466e7
Rohan Karak <karakrohan2005@gmail.com>
2026-08-21 00:06:07 +0530

    Time: 3 ms (43.88%) | Memory: 8.6 MB (64.95%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool isPalindrome(int x) {
        long long  int d=x,m=0,z;
-		for (int i = x; i > 0; i=i/10)
+		for (int i=x; i>0; i=i/10)
 		{
             z=i%10;
 	        m=m*10+z;
