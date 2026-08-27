f67bdae28b717daeafe2229c80167fd40c6a6787
Rohan Karak <karakrohan2005@gmail.com>
2026-08-28 01:50:51 +0530

    Time: 1218 ms (7.43%) | Memory: 28.2 MB (75.65%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int candidate=0;
-        int count=0;
-        for (int i=0;i<nums.size();i++){
-            if (count==0)candidate=nums[i];
-            if (nums[i]==candidate) count++;
-            else count--;
+        int n = nums.size();
+        //if n is even -> n/2
+        for(int i=0;i<n;i++){
+            int count = 1;
+            for(int j=i+1;j<n;j++){
+                if(nums[i]==nums[j]) count++;
+            }
+            if(count>(n/2)) return nums[i];
         }
-        return candidate;
+        return -1;
     }
 };
