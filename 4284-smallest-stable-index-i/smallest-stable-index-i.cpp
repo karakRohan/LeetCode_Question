6f7e63ed04d75174509875e91c05a54f3532f1cd
Rohan Karak <karakrohan2005@gmail.com>
2026-09-04 20:45:43 +0530

    Time: 0 ms (100.00%) | Memory: 30.4 MB (76.60%) - LeetSync

--- 4284-smallest-stable-index-i/smallest-stable-index-i.cpp
+++ 4284-smallest-stable-index-i/smallest-stable-index-i.cpp
@@ -4,20 +4,12 @@ public:
         int n=nums.size();
 
         for(int i=0;i<n;i++){
+            
             int largest=nums[0];
             int smallest=nums[i];
-
-            for(int j=0;j<=i;j++){
-                largest=max(largest,nums[j]);
-            }
-            
-            for(int j=i;j<n;j++){
-                smallest=min(smallest,nums[j]);
-            }
-
-            if(largest-smallest <= k){
-                return i;
-            }
+            for(int j=0;j<=i;j++) largest=max(largest,nums[j]);
+            for(int j=i;j<n;j++) smallest=min(smallest,nums[j]); 
+            if(largest-smallest <= k) return i;
         }
         return -1;
     }
