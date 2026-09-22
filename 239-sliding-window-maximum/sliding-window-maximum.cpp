6e81b7ff4a7fb73678222f885a650960b540bf7f
Rohan Karak <karakrohan2005@gmail.com>
2026-09-23 01:39:33 +0530

    Time: 27 ms (43.00%) | Memory: 139.1 MB (73.43%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -1,42 +1,24 @@
-// PW Solution
 class Solution {
 public:
-    vector<int> maxSlidingWindow(vector<int>& arr, int k) {
-        if (k == 1)
-            return arr;
-        int n = arr.size();
-        int ngi[n]; // ngi = Next Grater Index
-        stack<int> stack1;
-        ngi[n - 1] = n;
-        stack1.push(n - 1);
-        for (int i = n - 1; i >= 0; i--) {
-            // POP all the elements smaller than or equal to arr[i]
-            while (stack1.size() > 0 && arr[stack1.top()] <= arr[i]) {
-                stack1.pop();
-            }
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        deque<int>dq;
+        vector<int>ans;
 
-            // ANS [Mark the ANS is nge array]
-            if (stack1.size() == 0)
-                ngi[i] = n;
-            else
-                ngi[i] = stack1.top();
-
-            // PUSH the arr[i] into the stack
-            stack1.push(i);
-        }
-        vector<int> ans;
-        int j = 0;
-        for (int i = 0; i < n - k + 1; i++) {
-            if (j < i)
-                j = i;
-            int mx = arr[j];    // Starting Of Window Se
-            while (j < i + k) { // means if nge is inside window
-                mx = arr[j];
-                if (ngi[j] >= i + k)
-                    break;
-                j = ngi[j];
+        for (int i=0; i<nums.size(); i++) {
+            // Remove elements outside the window
+            if (!dq.empty()&& dq.front() <= i - k) {
+                dq.pop_front();
             }
-            ans.push_back(mx);
+            // Maintain decreasing order
+            while (!dq.empty()&& nums[dq.back()]<=nums[i]) {
+                dq.pop_back();
+            }
+            // Add current index
+            dq.push_back(i);
+            // Add maximum of current window
+            if (i>=k-1) {
+                ans.push_back(nums[dq.front()]);
+            }
         }
         return ans;
     }
